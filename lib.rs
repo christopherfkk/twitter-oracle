2f71309638826912adbe6c3a3803a301f98c6b11
christopherfkk <christopherfkk@gmail.com>
2022-07-19 11:53:15 +0800

    Passed json deserialize test

--- lib.rs
+++ lib.rs
@@ -1,6 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(trace_macros)]
 
+#[macro_use]
+extern crate alloc;
+
 use fat_utils::attestation;
 use ink_env::AccountId;
 use ink_lang as ink;
@@ -28,13 +31,19 @@ mod twitter_oracle {
     use ink_prelude::vec;
     use ink_storage::traits::SpreadAllocate;
     use ink_storage::Mapping;
+    use ink_env;
     use pink::{http_get, PinkEnvironment};
     use scale::{Decode, Encode};
     use fat_utils::attestation;
 
     use ink_prelude::borrow::ToOwned;
-    use serde::Deserialize;
+    use serde::{Deserialize, Serialize};
     use serde_json_core;
+
+    fn from_debug(e: impl core::fmt::Debug) -> Vec<u8> {
+        ink_env::debug_println!("Error: {:?}", e);
+        format!("{:?}", e).into_bytes()
+    }
 
     #[ink(storage)]
     #[derive(SpreadAllocate)]
@@ -62,7 +71,8 @@ mod twitter_oracle {
         UsernameAlreadyInUse,
         AccountAlreadyInUse,
         FailedToIssueBadge,
-        InvalidBody
+        InvalidBody,
+        InvalidTweets
     }
 
     impl TwitterOracle {
@@ -85,33 +95,6 @@ mod twitter_oracle {
     impl SubmittableOracle for TwitterOracle {
 
         #[ink(message)]
-        fn attest(&self, url: String) -> core::result::Result<attestation::Attestation, Vec<u8>> {
-
-            // Get username, tweet_id from "https://twitter.com/FokChristopher/status/1546748557595930625"
-            let tweet_url = parse_tweet_url(&url).map_err(|e| e.encode())?;
-
-            // Format API url with tweet_id in "https://api.twitter.com/2/tweets?ids={id}"
-            let mut req_url: String = "https://api.twitter.com/2/tweets?ids=".to_owned();
-            let tweet_id: &str = &tweet_url.tweet_id;
-            req_url.push_str(tweet_id);
-
-            // Fetch the tweet content: curl "https://api.twitter.com/2/tweets?ids=1426724855672541191" -H "Authorization: Bearer $BEARER_TOKEN"
-            let bearer_token: String = "Bearer AAAAAAAAAAAAAAAAAAAAACXsegEAAAAAmmADAF97nZBWgu1JDKG8ALb6lf8%3DduplCmqITqrQcjsIkovyPPbsu5WY6GNrcjsamf61obQrkJbE44".to_string();
-            let headers: Vec<(String, String)> = vec![("Authorization".into(), bearer_token)];
-            let response = http_get!(req_url, headers);
-            if response.status_code != 200 {
-                return Err(Error::RequestFailed.encode());
-            }
-
-            // TODO: Parse JSON body {"data": [{"id": <tweet_id>, "text": <tweet_content>}], e.g. {"data":[{"id":"1426724855672541191","text":"This tweet belongs to address:... "}]}
-            let body = response.body;
-            let account_id = extract_claim(&body).map_err(|e| e.encode())?;
-            let content = TweetContent { username: tweet_url.username, account_id, };
-            let result = self.attestation_generator.sign(content);
-            Ok(result)
-        }
-
-        #[ink(message)]
         fn admin(&self) -> AccountId {
             self.admin.clone()
         }
@@ -121,25 +104,61 @@ mod twitter_oracle {
         fn verifier(&self) -> attestation::Verifier {
             self.attestation_verifier.clone()
         }
+
+        #[ink(message)]
+        fn attest(&self, url: String) -> core::result::Result<attestation::Attestation, Vec<u8>> {
+
+            // Get username, tweet_id from "https://twitter.com/FokChristopher/status/1546748557595930625"
+            let tweet_url = parse_tweet_url(&url).map_err(|e| e.encode())?;
+            // ink_env::debug_println!("Tweet URL: {:?}", tweet_url);
+
+            // Format API url with tweet_id in "https://api.twitter.com/2/tweets?ids={id}"
+            let mut api_url: String = "https://api.twitter.com/2/tweets?ids=".to_owned();
+            let tweet_id: &str = &tweet_url.tweet_id;
+            api_url.push_str(tweet_id);
+            // ink_env::debug_println!("API URL {:?}", api_url);
+
+            // Fetch: curl "https://api.twitter.com/2/tweets?ids=1426724855672541191" -H "Authorization: Bearer $BEARER_TOKEN"
+            let bearer_token: String = "Bearer AAAAAAAAAAAAAAAAAAAAACXsegEAAAAAmmADAF97nZBWgu1JDKG8ALb6lf8%3DduplCmqITqrQcjsIkovyPPbsu5WY6GNrcjsamf61obQrkJbE44".to_string();
+            let headers: Vec<(String, String)> = vec![("Authorization".into(), bearer_token)];
+            // ink_env::debug_println!("API headers {:?}", headers);
+
+            let response = http_get!(api_url, headers);
+            // ink_env::debug_println!("Status code: {}", response.status_code);
+            if response.status_code != 200 {
+                return Err(Error::RequestFailed.encode());
+            }
+            // ink_env::debug_println!("Response body: {:?}", std::str::from_utf8(&response.body).unwrap());
+
+            // "data": [{"id": <tweet_id>, "text": <tweet>}]
+            let data: TweetData = serde_json_core::from_slice(&response.body)
+                .map_err(from_debug)?
+                .0;
+
+            let account_id = extract_claim(&data).map_err(|e| e.encode())?;
+            ink_env::debug_println!("Account ID: {:?}", account_id);
+
+            let quote = TweetQuote { username: tweet_url.username, account_id, };
+            let result = self.attestation_generator.sign(quote);
+            Ok(result)
+        }
     }
 
-    // TODO: Create struct for JSON to deserialize into {"data": [{"id": <tweet_id>, "text": <tweet_content>}]}
-    #[derive(Deserialize, Debug)]
-    pub struct Data<'a> {
+    #[derive(Deserialize, Debug, Serialize)]
+    pub struct TweetData<'a> {
         #[serde(borrow)]
-        data: Vec<Params<'a>>
+        data: Vec<TweetDataParams<'a>>
     }
 
-    // TODO: Create struct for JSON to deserialize into {"data": [{"id": <tweet_id>, "text": <tweet_content>}]}
-    #[derive(Deserialize, Debug)]
-    pub struct Params<'a> {
+    #[derive(Deserialize, Debug, Serialize)]
+    pub struct TweetDataParams<'a> {
         id: &'a str,
         text: &'a str
     }
 
     #[derive(Clone, Encode, Decode, Debug)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-    pub struct TweetContent {
+    pub struct TweetQuote {
         username: String,
         account_id: AccountId,
     }
@@ -167,20 +186,18 @@ mod twitter_oracle {
     const CLAIM_PREFIX: &str = "This tweet is owned by address: 0x";
     const ADDRESS_LEN: usize = 64;
 
-    fn extract_claim(body: &[u8]) -> Result<AccountId, Error> {
-
-        // TODO: extract actual tweet from bytes of JSON text
-        let (data, _): (Data, usize) =
-                serde_json_core::from_slice(body).or(Err(Error::InvalidBody))?;
-        let text: &str = data.data[0].text;
-
+    fn extract_claim(data: &TweetData) -> Result<AccountId, Error> {
+        let text = data.data[0].text.to_string();
+        ink_env::debug_println!("{:?}", text);
         let pos = text.find(CLAIM_PREFIX).ok_or(Error::NoClaimFound)?;
+        ink_env::debug_println!("{:?}", pos);
         let addr: String = text
             .chars()
             .skip(pos)
             .skip(CLAIM_PREFIX.len())
             .take(ADDRESS_LEN)
             .collect();
+        ink_env::debug_println!("{:?}", addr);
         let addr: &[u8] = addr.as_bytes();
         let account_id = decode_account_id_256(addr)?;
         Ok(account_id)
@@ -219,28 +236,38 @@ mod twitter_oracle {
         #[ink::test]
         fn can_decode_claim() {
 
-            // Test JSON response from twitter API
-            let json: &str = r#"{"{"data":[{"id":"1426724855672541191","text":"This tweet belongs to address: 0123456789012345678901234567890123456789012345678901234567890123"}]}"#;
-            let body = json.as_bytes();
-
-            let ok: Result<AccountId, Error> = extract_claim(body);
+            let params = TweetDataParams {
+                id: "1426724855672541191",
+                text: "This tweet is owned by address: 0x0123456789012345678901234567890123456789012345678901234567890123"
+            };
+            let data = TweetData { data: vec![params] };
+            let ok: Result<AccountId, Error> = extract_claim(&data);
             assert_eq!(
                 ok,
                 decode_account_id_256(
                     b"0123456789012345678901234567890123456789012345678901234567890123"
                 )
             );
+
             // Bad cases
+            let params = TweetDataParams { id: "1426724855672541191", text: "This tweet is owned by address:" };
+            let data = TweetData { data: vec![params] };
             assert_eq!(
-                extract_claim(b"This tweet is owned by"),
+                extract_claim(&data),
                 Err(Error::NoClaimFound),
             );
+
+            let params = TweetDataParams { id: "1426724855672541191", text: "This tweet is owned by address: 0xAB" };
+            let data = TweetData { data: vec![params] };
             assert_eq!(
-                extract_claim(b"This tweet is owned by address: 0xAB"),
+                extract_claim(&data),
                 Err(Error::InvalidAddressLength),
             );
+
+            let params = TweetDataParams { id: "1426724855672541191", text: "This tweet is owned by address: 0xXX23456789012345678901234567890123456789012345678901234567890123" };
+            let data = TweetData { data: vec![params] };
             assert_eq!(
-                extract_claim(b"This tweet is owned by address: 0xXX23456789012345678901234567890123456789012345678901234567890123"),
+                extract_claim(&data),
                 Err(Error::InvalidAddress),
             );
         }
@@ -253,18 +280,19 @@ mod twitter_oracle {
             fat_utils::test_helper::mock_all();
             let accounts: DefaultAccounts<PinkEnvironment> = default_accounts();
 
-            let mut contract = TwitterOracle::new();
+            let contract = TwitterOracle::new();
 
-            mock::mock_http_request(|_| {
-                HttpResponse::ok(
-                    b"This tweet is owned by address: 0x0101010101010101010101010101010101010101010101010101010101010101".to_vec())
-            });
+            // Test JSON response from twitter API
+            let json: &str = r#"{"data":[{"id":"1426724855672541191","text":"This tweet is owned by address: 0x0101010101010101010101010101010101010101010101010101010101010101"}]}"#;
+            let body = json.as_bytes();
+            mock::mock_http_request(|_| { HttpResponse::ok(body.to_vec()) });
+
             let result = contract.attest(
                 "https://twitter.com/FokChristopher/status/1546748557595930625".to_string());
             assert!(result.is_ok());
 
             let attestation = result.unwrap();
-            let data: TweetContent = Decode::decode(&mut &attestation.data[..]).unwrap();
+            let data: TweetQuote = Decode::decode(&mut &attestation.data[..]).unwrap();
             assert_eq!(data.username, "FokChristopher");
             assert_eq!(data.account_id, accounts.alice);
         }
